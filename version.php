6abb3553b278ed2b7926f9f7a8e944c4a25e779d
Alexander D <alexander.dominicus@rub.de>
2022-01-28 11:04:24 +0100

    Changed version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_responsim';
-$plugin->release = '0.1.0';
-$plugin->version = 2021121401;
+$plugin->release = '0.2.0';
+$plugin->version = 2022012801;
 $plugin->requires = 2017111300;
 $plugin->maturity = MATURITY_ALPHA;
